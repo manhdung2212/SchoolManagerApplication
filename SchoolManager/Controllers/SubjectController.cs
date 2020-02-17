552ee0bb78972c8c68e9e3840f747067a2964b99
unknown <khuatchung1012@gmail.com>
2020-02-17 17:00:21 +0700

    abc

---
+++ SchoolManager/Controllers/SubjectController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManager.Models;
+
+namespace SchoolManager.Controllers
+{
+    public class SubjectController : Controller
+    {
+        SchoolManagementEntities db = new SchoolManagementEntities();
+        // GET: Subject
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Listsubject(int pageNumber , int pageSize,string search)
+        {
+            var data = db.Subjects.OrderBy(x => x.Name);
+                       
+                        
+            if (search.Trim() != "")
+            {
+                    data = data.Where(x => x.Name.Contains(search)).OrderBy(x=>x.Name);
+            }
+            var pageCount = data.Count() % pageSize == 0 ? data.Count() / pageSize : data.Count() / pageSize + 1;
+            if (pageNumber <= pageCount)
+            {
+                var model = data.OrderBy(x => x.Name).Skip(pageSize * pageNumber - pageSize).Take(pageSize).ToList();
+                ViewBag.pageCount = pageCount;
+                ViewBag.pageNumber = pageNumber;
+                return PartialView(model);
+            }
+            return PartialView(data.OrderBy(x => x.Name));
+        }
+        public PartialViewResult FormCreateEdit()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult DetailInfo(int id)
+        {
+            Subject sb = db.Subjects.Find(id);
+            return PartialView(sb);
+        }
+
+        [HttpPost]
+        public JsonResult Create(string Name,string Node,int Status)
+        {
+            Subject sb = new Subject();
+            sb.Name = Name;
+            sb.Node = Node;
+            sb.CreateDate = DateTime.Now;
+            sb.UpdateDate = DateTime.Now;
+            
+            sb.Status = Status;
+            db.Subjects.Add(sb);
+            db.SaveChanges();
+            return Json(true);
+        }
+
+        [HttpPost]
+        public JsonResult Update(int id,string Name, string Node, int Status)
+        {
+            Subject sb = db.Subjects.Find(id);
+            sb.Name = Name;
+            sb.Node = Node;
+            sb.UpdateDate = DateTime.Now;
+           
+            sb.Status = Status;
+            db.SaveChanges();
+            return Json(true);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        { 
+            Subject sb = db.Subjects.Find(id);
+            db.Subjects.Remove(sb);
+            db.SaveChanges();
+            return Json(true);
+        }
+        
+        public ActionResult Detail(int id)
+        {
+            var sb = db.Subjects.Find(id);
+            return View(sb);
+        }
+    }
+}
---
+++ SchoolManager/Scripts/SubjectManager.js
@@ -0,0 +1,232 @@
+﻿$(document).ready(function () {
+    var id = 0;
+    var pageNumber = 1;
+    var pageSize = 5;
+    var search = "";
+    GetListSubject();
+    Create();
+    EditClick();
+    Update();
+    Delete();
+    Pagination();
+    Search();
+    
+    function GetListSubject() {
+        $.ajax({
+            url: "/Subject/Listsubject",
+            type: "Get",
+            dataType: "html",
+            data: {
+                pageSize: pageSize,
+                pageNumber: pageNumber,
+                search: search,
+                
+            },
+            beforSend: function () {
+
+            },
+            success: function (res) {
+                $('.list-subject').html('');
+                $('.list-subject').append(res);
+                EditClick();
+                DefaultValue();
+                Pagination();
+                Delete();
+                GetInfo();
+            },
+            error: function () {
+
+            },
+            complete: function () {
+
+            }
+        })
+    }
+
+    function Create() {
+        $('.btn-create').click(function () {
+            var Name = $('.add-or-edit input[name="Name"]').val();
+            var Node = $('.add-or-edit input[name="Node"]').val();
+           
+            var Status = $('.add-or-edit input[name="Status"]').val();
+
+            if (CheckError(Name, Node, Status)) {
+                $.ajax({
+                    url: "/Subject/Create",
+                    type: "Post",
+                    dataType: "Json",
+                    data: {
+                        Name: Name,
+                        Node: Node,
+                     
+                        Status: Status,
+                    },
+                    beforSend: function () {
+
+                    },
+                    success: function (res) {
+                        GetListSubject();
+                    },
+                    error: function () {
+
+                    },
+                    complete: function () {
+
+                    }
+                })
+            }
+            
+
+        })
+    }
+
+    function EditClick() {
+        $('.btn-edit').click(function () {
+            id = $(this).attr('data-id');
+            var name = $(this).attr('data-name');
+            var node = $(this).attr('data-node');
+           
+            
+            var status = $(this).attr('data-status');
+
+
+            $('.add-or-edit input[name="Name"]').val(name);
+            $('.add-or-edit input[name="Node"]').val(node);
+           
+            
+            $('.add-or-edit input[name="Status"]').val(status);
+        })
+    }
+
+    function Update() {
+        $('.btn-update').click(function () {
+            var Name = $('.add-or-edit input[name="Name"]').val();
+            var Node = $('.add-or-edit input[name="Node"]').val();
+           
+            var Status = $('.add-or-edit input[name="Status"]').val();
+
+            $.ajax({
+                url: "/Subject/Update",
+                type: "Post",
+                dataType: "Json",
+                data: {
+                    id : id,
+                    Name: Name,
+                    Node: Node,
+                    
+                    Status: Status,
+                },
+                beforSend: function () {
+
+                },
+                success: function (res) {
+                    GetListSubject();
+                },
+                error: function () {
+
+                },
+                complete: function () {
+
+                }
+            })
+        })
+    }
+
+    function Delete() {
+        $('.btn-delete').click(function () {
+            var id = $(this).attr('data-id');
+            $.ajax({
+                url: "/Subject/Delete",
+                type: "Post",
+                dataType: "Json",
+                data: {
+                    id: id,
+                    
+                },
+                beforSend: function () {
+
+                },
+                success: function (res) {
+                    GetListSubject();
+                },
+                error: function () {
+
+                },
+                complete: function () {
+
+                }
+            })
+        })
+    }
+    function DefaultValue() {
+        $('.add-or-edit input[name="Name"]').val('');
+        $('.add-or-edit input[name="Node"]').val('');
+        $('.add-or-edit input[name="Status"]').val('');
+    }
+    
+    function Pagination() {
+        $('.pagination button').click(function () {
+            pageNumber = $(this).attr('data-page');
+            GetListSubject();
+        })
+    }
+    
+    function Search() {
+        $('input[name="searchname"]').keyup(function () {
+            search = $(this).val();
+            GetListSubject();
+        })
+    }
+
+    GetInfo();
+    function GetInfo() {
+        $('.btn-detail').click(function () {
+            
+            var id = $(this).attr('data-id');
+            $.ajax({
+                url: "/Subject/DetailInfo",
+                type: "get",
+                dataType: "html",
+                data: {
+                    id: id,
+
+                },
+                beforSend: function () {
+
+                },
+                success: function (res) {
+                    //$('.list-subject').html('');
+                    //$('.list-subject').append(res);
+                    $('.modal-body').html('');
+                    $('.modal-body').append(res);
+                    //GetInfo();
+                    //$('.btn-getlist').click(function () {
+                    //    GetListSubject();
+                    //})
+                },
+                error: function () {
+
+                },
+                complete: function () {
+
+                }
+            })
+        })
+    }
+    
+    function CheckError(name, node, status) {
+        if (name.trim() == '' || node.trim() == ''  || status.trim() == '') {
+            $('.error').html('');
+            $('.add-or-edit input').each(function () {
+                if ($(this).val().trim() == '') {
+                    var name = $(this).siblings('label').text();
+                    var row = `<p> ${name} không được để trống<p>`;
+                    $(this).siblings('.error').append(row);
+                }
+
+            })
+            return false;
+        }
+        return true;
+    }
+})
